3b63ae98b18a13464bc776eda0b1874e0351611d
Samuel Ibanga <ibangajnr27@gmail.com>
2022-10-19 21:15:03 +0100

    Created Models and Attributes for database operations

--- songcrud/songcrud/musicapp/models.py
+++ songcrud/songcrud/musicapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+class Song(models.Model):
+    title = models.CharField(max_length=255)
+    date_released = models.DateField(auto_now_add=True)
+    likes = models.BooleanField(default=False)
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+
+class Lyric(models.Model):
+    content = models.TextField(max_length=1000)
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
---
+++ songcrud/songcrud/musicapp/urls.py
@@ -0,0 +1,7 @@
+from turtle import home
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home),
+]
